792e82822abd6f95c6f6f31777224d84fb1c65aa
Lorenzo Bonin <104757900+Boni53@users.noreply.github.com>
2022-06-20 10:42:56 +0200

    Add files via upload

---
+++ magnetic_switch/magnetic_switch.ino
@@ -0,0 +1,56 @@
+/*******************************************************************************/
+ 
+/*macro definitions of magnetic pin and LED pin*/
+#define MAGNECTIC_SWITCH 0
+#define LED 13//the on board LED of the Arduino or Seeeduino
+ 
+void setup()
+{
+  Serial.begin(9600);
+ 
+  pinsInit();
+}
+ 
+void loop()
+{
+  if (isNearMagnet()) //if the magnetic switch is near the magnet?
+  {
+    Serial.println("no");
+ 
+    turnOnLED();
+  }
+  else
+  {
+    Serial.println("yes");
+ 
+    turnOffLED();
+  }
+}
+void pinsInit()
+{
+  pinMode(MAGNECTIC_SWITCH, INPUT);
+  pinMode(LED, OUTPUT);
+}
+ 
+/*If the magnetic switch is near the magnet, it will return ture, */
+/*otherwise it will return false                                */
+boolean isNearMagnet()
+{
+  int sensorValue = digitalRead(MAGNECTIC_SWITCH);
+  if (sensorValue == HIGH) //if the sensor value is HIGH?
+  {
+    return true;//yes,return ture
+  }
+  else
+  {
+    return false;//no,return false
+  }
+}
+void turnOnLED()
+{
+  digitalWrite(LED, HIGH);
+}
+void turnOffLED()
+{
+  digitalWrite(LED, LOW);
+}
